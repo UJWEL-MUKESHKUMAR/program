d30f73bf462dcdd96d1661615c46ab9c70297895
Ujwel mukeshkumar <62126395+UJWEL-MUKESHKUMAR@users.noreply.github.com>
2020-03-14 10:44:48 +0530

    Create Create hcf.c

---
+++ Create hcf.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+ Void main()
+   {
+      Int i, num1, num2, min, hcf=1;
+      printf("Enter two numbers \n");
+      scanf("%d%d",&num1, &num2); 
+      for(i=1;i<=min; i++)
+       {
+         /*if f is a factor of both */
+         if(num1%i==0&& num2%i==0)
+          {
+            hcf=i;
+           }
+       }
+   }
