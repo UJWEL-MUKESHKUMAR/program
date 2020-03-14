574a1297c320d7b7693829668a3eff5a02942935
Ujwel mukeshkumar <62126395+UJWEL-MUKESHKUMAR@users.noreply.github.com>
2020-03-14 10:50:05 +0530

    Create Create square numbers.c

---
+++ Create square numbers.c
@@ -0,0 +1,12 @@
+#include <stdio. h>
+ void main()
+  {
+    int i, n, sum=0;
+    printf("input the number of terms\n");
+    scanf("%d",&n); 
+    printf("\n The square of natural number is:");
+    for(i=1;i<=n; i++)
+     {
+       printf("%d",i*i);
+     }
+  }
