782b3e0faa76f31eafdd19b62d713bc7fceee159
Ujwel mukeshkumar <62126395+UJWEL-MUKESHKUMAR@users.noreply.github.com>
2020-03-14 10:51:31 +0530

    Create Create sinxcosx.c

---
+++ Create sinxcosx.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+ #include <math.h>
+ void main ()
+  {
+    float sine, cosine, x; 
+    printf("\n Enter the value :");
+    scanf("%f",&x);
+    sine=sin(x);
+    cosine=cos(x);
+    printf("\n sine value is %f",sine); 
+    Printf("cos value is %f",cosine); 
+ }
