d4122bb5961fb150d0f53dc57d5fe4200c480d95
Ujwel mukeshkumar <62126395+UJWEL-MUKESHKUMAR@users.noreply.github.com>
2020-03-14 10:52:41 +0530

    Create Create flodys triangle .c

---
+++ Create flodys triangle .c
@@ -0,0 +1,14 @@
+#include <stdio. h>
+ int main()
+  {
+    int row, I, j, num=1;
+    printf("Enter number of rows\n");
+    for(i=1;i<=rows; i++)
+     { 
+       for(j=1;j<=I;++j) 
+        {
+          printf("%d",num); 
+          ++num; 
+         }
+     }
+  } 
